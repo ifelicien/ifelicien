f0c716e46547368dd396670a0f994e81f8c9cd9f
Irwin Felicien <felicienirwin@outlook.com>
2024-02-19 13:18:33 -0600

    first commit

---
+++ my-react-webapp/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Felicien Software Solutions</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ my-react-webapp/src/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+
+    return (
+    <header>
+      <h1>Irwin Felicien</h1>
+    </header>
+  );
+}
+
+export default Header;
