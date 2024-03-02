74517f0d74710ed9d82b16f1bab6df39ad81d317
ifeli <felicienirwin>
2024-03-01 23:22:39 -0600

    changes to reacta and css files

--- my-react-webapp/src/App.jsx
+++ my-react-webapp/src/App.jsx
@@ -3,9 +3,9 @@ import Footer from "./Footer/Footer.jsx";
 import About from "./About/About.jsx";
 import Title from "./Title/Title.jsx";
 import Projects from "./Projects/Projects.jsx";
-// import Skills from './Footer/Skills.jsx'
-// import Education from './Footer/Education.jsx'
-// import Contacts from './Footer/Contacts.jsx'
+import Skills from './Skills/Skills.jsx'
+// import Education from './Education/Education.jsx'
+// import Contacts from './Contacts/Contacts.jsx'
 
 function App() {
   return (
@@ -14,17 +14,27 @@ function App() {
       <About />
       <Title sectionName="Solo-Projects" />
       <Projects
-        image="https://placehold.jp/200x200.png"
+        image="https://placehold.jp/250x250.png"
         name="Shop Tracker"
         summary="a small summary of the STS application"
       />
       <Projects
-        image="https://placehold.jp/200x200.png"
+        image="https://placehold.jp/250x250.png"
         name="Research Assistant"
         summary="a small summary of the Machine Learning Research"
       />
       <Title sectionName="Class-Projects" />
-      {/* <Skills/> */}
+      <Projects
+        image="https://placehold.jp/250x250.png"
+        name="CLI Vending Machine"
+        summary="a small summary of the Vending Machine Project"
+      />
+      <Projects
+        image="https://placehold.jp/250x250.png"
+        name="Capstone Project"
+        summary="Summary of Capstone Group Project"
+      />
+      <Skills/>
       {/* <Education/> */}
       {/* <Contacts/> */}
       <Footer />
---
+++ my-react-webapp/src/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import styles from "./Skills.module.css";
+
+function Skills (){
+    return (
+        <div className={styles.skills}>
+            <h2>Skills</h2>
+            {/* <img src="" alt="" srcset="" /> */}
+        </div>
+    )
+
+}
+export default Skills;
