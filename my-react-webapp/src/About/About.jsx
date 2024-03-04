526cb5d08439f6342a732ad429c737c3b49a65bc
ifeli <felicienirwin>
2024-03-04 12:04:26 -0600

    added logos

--- my-react-webapp/src/About/About.jsx
+++ my-react-webapp/src/About/About.jsx
@@ -1,9 +1,10 @@
 import styles from "./About.module.css";
+import headShot from "../assets/_DSC0009.png"
 // singular card spanning the width of the page
 function About() {
   return (
     <div className={styles.about}>
-      <img src="https://placehold.jp/250x250.png" alt="" />
+      <img src={headShot} alt="" />
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, quisquam,
         voluptate quis id necessitatibus magni reprehenderit sunt consectetur at
--- my-react-webapp/src/Skills/Skills.jsx
+++ my-react-webapp/src/Skills/Skills.jsx
@@ -1,11 +1,23 @@
 import styles from "./Skills.module.css";
-import reactLogo from '../assets/react.svg';
+import reactLogo from "../assets/react.svg";
+import htmlLogo from "../assets/HTML logo.png";
+import CSSLogo from "../assets/CSS_logo.png";
+import cSharpLogo from "../assets/Csharp_Logo.png";
+import BlazorLogo from "../assets/BlazorLogo.png";
+import JavaLogo from "../assets/Java-Logo.png";
 
 function Skills() {
   return (
-    <div className={styles.skills}>
-      <img src={reactLogo} alt="React Logo" />
-    </div>
+    <>
+      <div className={styles.skills}>
+        <img src={reactLogo} alt="React Logo" />
+        <img src={htmlLogo} alt="HTML Logo" />
+        <img src={CSSLogo} alt="CSS Logo" />
+        <img src={cSharpLogo} alt="C# Logo" />
+        <img src={BlazorLogo} alt="Blazor Logo" />
+        <img src={JavaLogo} alt="Blazor Logo" />
+      </div>
+    </>
   );
 }
 export default Skills;
